d015f9be8a977952e698d74ec720764e865a4615
francescobianco <info.francescobianco@gmail.com>
2017-02-18 16:06:06 +0100

    version 0.0.3

--- src/Producer.php
+++ src/Producer.php
@@ -1,13 +1,58 @@
 <?php
+/**
+ *
+ *
+ */
 
 namespace Javanile;
 
 class Producer {
-    
-    public static function cli() {
 
-        echo "Producer\n";
-        
+    /**
+     *
+     *
+     */
+    public static function cli() {
+        global $argv;
+        $producer = new Producer();
+        $producer->run(array_slice($argv,1));
     }
-    
+
+    /**
+     *
+     *
+     */
+    private function run($args) {
+
+        //
+        if (!isset($args[0])) {
+
+        }
+
+        //
+        $cmd = $args[0];
+
+        //
+        switch ($cmd) {
+            case 'publish': return $this->cmdPublish($args);
+        }
+    }
+
+    /**
+     *
+     */
+    private function cmdPublish($args) {
+
+        //
+        $cwd = getcwd();
+
+        //
+        $rep = $args[1];
+
+        //
+        $out = shell_exec(__DIR__.'/../exec/publish.sh '.$cwd.' '.$rep);
+
+        //
+        echo $out;
+    }
 }
