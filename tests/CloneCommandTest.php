c240c5f7b9462fbf6ca34c5e01c54a27d4a5b3e4
Francesco Bianco <info.francescobianco@gmail.com>
2018-07-09 23:33:34 +0200

    fix

--- tests/CloneCommandTest.php
+++ tests/CloneCommandTest.php
@@ -2,11 +2,10 @@
 
 namespace Javanile\Producer\Tests;
 
-use Javanile\Producer;
 use Javanile\Producer\Commands\CloneCommand;
 use PHPUnit\Framework\TestCase;
 
-Producer::addPsr4(['Javanile\\Producer\\Tests\\' => __DIR__]);
+//Producer::addPsr4(['Javanile\\Producer\\Tests\\' => __DIR__]);
 
 final class CloneCommandTest extends TestCase
 {
@@ -14,7 +13,7 @@ final class CloneCommandTest extends TestCase
 
     public function testCloneGitHubProject()
     {
-        $clone = new CloneCommand(__DIR__.'/cwd');
+        $clone = new CloneCommand(__DIR__.'/temp');
         //$clone->run(['https://github.com/javanile/urlman']);
         //$this->assertDirectoryExists(__DIR__.'/cwd/repository/urlman');
         //$this->assertDirectoryExists(__DIR__.'/cwd/vendor/javanile/urlman');
@@ -22,7 +21,7 @@ final class CloneCommandTest extends TestCase
 
     public function testCloneGitHubEmptyProject()
     {
-        $clone = new CloneCommand(__DIR__.'/cwd');
+        $clone = new CloneCommand(__DIR__.'/temp');
         $clone->run(['https://github.com/php-source-code/simple-psr-1']);
         $this->assertDirectoryExists(__DIR__.'/cwd/repository/simple-psr-1');
         $this->assertDirectoryExists(__DIR__.'/cwd/vendor/php-source-code/simple-psr-1');
