fe2a4ab59184d1e825a2ff77cfd23da5d9f833cc
Francesco Bianco <info.francescobianco@gmail.com>
2018-07-11 17:29:21 +0200

    fix

--- tests/Commands/CloneCommandTest.php
+++ tests/Commands/CloneCommandTest.php
@@ -6,25 +6,30 @@ use Javanile\Producer\Commands\CloneCommand;
 use Javanile\Producer\Tests\CwdTrait;
 use PHPUnit\Framework\TestCase;
 
-//Producer::addPsr4(['Javanile\\Producer\\Tests\\' => __DIR__]);
-
 final class CloneCommandTest extends TestCase
 {
     use CwdTrait;
 
     public function testCloneByRepository()
     {
-        // Testing
         $clone = new CloneCommand($this->getCwd());
-        $clone->run(['https://github.com/php-code-samples/simple-psr-1']);
-        $this->assertDirectoryExists(__DIR__.'/cwd/packages/simple-psr-1');
-        $this->assertDirectoryExists(__DIR__.'/cwd/vendor/php-source-code/simple-psr-1');
+        $clone->run(['https://github.com/php-code-samples/package-skeleton']);
+        $this->assertDirectoryExists(__DIR__.'/cwd/packages/package-skeleton');
+        $this->assertDirectoryExists(__DIR__.'/cwd/vendor/php-code-samples/package-skeleton');
     }
 
     public function testCloneByPackageName()
     {
         $clone = new CloneCommand($this->getCwd());
-        $clone->run(['php-code-samples/simple-psr-1']);
+        $clone->run(['php-code-samples/package-skeleton']);
+        $this->assertDirectoryExists(__DIR__.'/cwd/packages/package-skeleton');
+        $this->assertDirectoryExists(__DIR__.'/cwd/vendor/php-code-samples/package-skeleton');
+    }
+
+    public function testCloneByRepository()
+    {
+        $clone = new CloneCommand($this->getCwd());
+        $clone->run(['https://github.com/php-code-samples/package-skeleton']);
         $this->assertDirectoryExists(__DIR__.'/cwd/packages/simple-psr-1');
         $this->assertDirectoryExists(__DIR__.'/cwd/vendor/php-source-code/simple-psr-1');
     }
